94d7b226c3840c4b64c256dd876c60f88898ee5a
VS <xxrrrxx@yandex.ru>
2017-09-16 19:58:24 +0300

    dev-env-setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App'
+
+import reducers from './reducers/reduceindex';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
