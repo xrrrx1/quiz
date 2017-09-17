638a5e620436d7c3a466225745e0e6ac0d5171aa
VS <xxrrrxx@yandex.ru>
2017-09-17 14:48:56 +0300

    header-styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
+import Header from './header'
+
 export default class App extends Component {
     render() {
         return (
            <div className={"app"}>
-               HEADER
+               <Header/>
                <div className="container">
                    <h3>
                        {this.props.children}
                    </h3>
                </div>
-               FOOTER
            </div>
         );
     }
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="container">
+                <div className="brand">
+                    Quiz game
+                </div>
+                </div>
+            </div>
+        );
+    }
+}
